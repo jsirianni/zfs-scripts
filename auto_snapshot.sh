28064e10399a4a814ecc72a50f4eb287c19773ae
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-04-18 23:55:56 -0400

    Fix bad repo

--- auto_snapshot.sh
+++ auto_snapshot.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 cd $(dirname $0)
-
 #
-# Replace "zpool" with the name of your zpool
-#
+# Replace "tank" with the name of your zpool
 # Replace "dataset" with the name of your dataset
+# Additional datasets can be added by copying the dataset section
+# This script installs auto snapshots and enalbles them globally
+# Auto snapshots are not actually taken unless you change false to true for each snapshot type
 #
-# Datasets can be added by copying lines 20-26 and replacing the dataset names
+# View snapshot information with 'sudo zfs get all | grep snapshot'
 #
 
 
+#
+# Install required packages
+#
+sudo apt-get install unzip make
+
+
+#
 # Add repo and install auto snapshots
-sudo add-apt-repository ppa:bob-ziuchkovski/zfs-auto-snapshot
-sudo apt-get update
-sudo apt-get install -y zfs-auto-snapshot
+#
+sudo wget https://github.com/jsirianni/zfs-autosnapshot-packages/raw/master/zfs-auto-snapshot-master.zip -P /tmp
+sudo unzip /tmp/zfs-auto-snapshot-master.zip -d /tmp/
+sudo make install -C /tmp/zfs-auto-snapshot-master
 
 
-# Disable top level snapshots & enable weekly, daily, hourly, frequent snapshots for mounted zpool
-sudo zfs set com.sun:auto-snapshot=false zpool
-sudo zfs set com.sun:auto-snapshot=true zpool/dataset
-sudo zfs set com.sun:auto-snapshot:monthly=false zpool/dataset
-sudo zfs set com.sun:auto-snapshot:weekly=true zpool/dataset
-sudo zfs set com.sun:auto-snapshot:daily=true zpool/dataset
-sudo zfs set com.sun:auto-snapshot:hourly=true zpool/dataset
-sudo zfs set com.sun:auto-snapshot:frequent=false zpool/dataset
+#
+# Enable top level snapshots, and then disable each type individually
+#
+sudo zfs set com.sun:auto-snapshot=true tank
+sudo zfs set com.sun:auto-snapshot:monthly=false tank
+sudo zfs set com.sun:auto-snapshot:weekly=false tank
+sudo zfs set com.sun:auto-snapshot:daily=false tank
+sudo zfs set com.sun:auto-snapshot:hourly=false tank
+sudo zfs set com.sun:auto-snapshot:frequent=false tank
+
+
+#
+# Enable dataset level snapshots, and then disable each type invididually 
+#
+sudo zfs set com.sun:auto-snapshot=true tank/dataset
+sudo zfs set com.sun:auto-snapshot:monthly=false tank/dataset
+sudo zfs set com.sun:auto-snapshot:weekly=false tank/dataset
+sudo zfs set com.sun:auto-snapshot:daily=false tank/dataset
+sudo zfs set com.sun:auto-snapshot:hourly=false tank/dataset
+sudo zfs set com.sun:auto-snapshot:frequent=false tank/dataset
